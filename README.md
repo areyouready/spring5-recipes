7d7d7051262310c5462de594577367dd533eb95a
Sebastian Basner <open.sebastian@gmail.com>
2019-04-23 22:34:21 +0200

    Initial project setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # spring5-recipes
 A recipe application using Spring Boot 2 &amp; Spring 5
+
+The UI is build with Thymeleaf 
---
+++ src/main/java/de/fnortheim/spring5recipes/Spring5RecipesApplication.java
@@ -0,0 +1,13 @@
+package de.fnortheim.spring5recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecipesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5RecipesApplication.class, args);
+    }
+
+}
