2cd87e275a2b79cad178fe49f9f5f542654ed622
Sebastian Basner <open.sebastian@gmail.com>
2019-06-11 00:24:39 +0200

    Added IntegrationTest with DataJpaTest and MockMVC Test

---
+++ src/test/java/de/fnortheim/spring5recipes/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,44 @@
+package de.fnortheim.spring5recipes.repositories;
+
+import de.fnortheim.spring5recipes.domain.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * created by sebastian on Jun, 2019
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest //Starts embedded Database and configures Spring Data JPA, no servlet container is used everything regarding that and MVC is mocked out It is a slice of @SpringBootTest
+public class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void findByDescription() throws Exception {
+
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", unitOfMeasure.get().getDescription());
+    }
+
+    @Test
+    public void findByDescriptionCup() throws Exception {
+
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Cup");
+
+        assertEquals("Cup", unitOfMeasure.get().getDescription());
+    }
+}
